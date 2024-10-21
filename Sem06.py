24f958d160187bd9222583dd8ae975f7eab9a34f
Crisz04 <criswalkz04@gmail.com>
2024-10-21 12:14:52 -0500

    Agregando un nuevo ejercicio

--- Sem06.py
+++ Sem06.py
@@ -10,3 +10,10 @@ st.subheader("Ejercicio 1: Imprimir 10 veces 'Hola mundo'")
 if st.button("Ejecutar ejercicio 1"):
     for i in range(10):
         st.write("Hola Mundo")
+
+#Ejercicio 2: Imprimir los primeros 10 numeros
+
+st.title("Ejercicio 2: 'Imprimir los 10 tprimeros numeros'")
+
+if i in range(1, 11):
+    st.write(i)
