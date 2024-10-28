64893d4d5d48b1b15c5b76690a231bb7bde7e1e1
Crisz04 <criswalkz04@gmail.com>
2024-10-27 20:33:43 -0500

    alineando sangria

--- sem07.py
+++ sem07.py
@@ -6,16 +6,16 @@ def mostar_menu():
     menus = ["archivo", "Editar", "Ver", "Salir"]
     seleccion = ""
 
-        seleccion = st.radio("Menu", menus)
+    seleccion = st.radio("Menu", menus)
 
-        if seleccion == "Archivo":
-            st.write("Seleccionaste Archivo")
-        elif seleccion == "Editar":
-            st.write("Seleccionaste editar")
-        elif seleccion == "Ver":
-            st.write("Seleccionaste: Ver")
-        elif seleccion == "Salir":
-            st.write("¡Saliendo sel Menu!")
+    if seleccion == "Archivo":
+        st.write("Seleccionaste Archivo")
+    elif seleccion == "Editar":
+        st.write("Seleccionaste editar")
+    elif seleccion == "Ver":
+        st.write("Seleccionaste: Ver")
+    elif seleccion == "Salir":
+        st.write("¡Saliendo sel Menu!")
 
 if __name__ == "__main__":
     mostar_menu()
