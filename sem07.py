901b35ba83e259dce1d75b64d2d2ba66e307a254
Crisz04 <criswalkz04@gmail.com>
2024-10-27 20:27:36 -0500

    ejercicio 1

---
+++ sem07.py
@@ -0,0 +1,23 @@
+import streamlit as st
+def mostar_menu():
+    st.title("Ejemplo de menu")
+    st.write("selecciona una opcion del menú")
+
+    menu = ["archivo", "Editar", "Ver", "Salir"]
+    seleccion = ""
+
+    while seleccion != "salir":
+        seleccion = st.radio("Menu", menu)
+
+        if seleccion == "Archivo":
+            st.write("Seleccionaste Archivo")
+        elif seleccion == "Editar":
+            st.write("Seleccionaste editar")
+        elif seleccion == "Ver":
+            st.write("Seleccionaste: Ver")
+        elif seleccion == "Salir":
+            st.write("¡Saliendo sel Menu!")
+            break
+
+if __name__ == "__main__":
+    mostar_menu()
