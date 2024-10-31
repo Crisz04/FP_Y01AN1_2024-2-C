02b6f8daf60e05e365c3b9757546e71c54cbe3c5
Crisz04 <criswalkz04@gmail.com>
2024-10-31 15:48:29 -0500

    insertando subtitulo

--- Ejercicios_IDL3.py
+++ Ejercicios_IDL3.py
@@ -2,3 +2,4 @@
 import streamlit as st
 
 st.title("EJERCICIOS PROPUESTOS PARA EL CASO PROPUESTO IDL 3")
+st.bubheader("Ejercicio 1: Crear una calculadora de las cuatro operaciones básicas, donde se envíe los parámetros a operar y este retorne los resultados")
