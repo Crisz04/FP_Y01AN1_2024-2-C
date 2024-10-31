e06fd5023f1cc5ac14de9b4f93b36bdf0a6bc1f6
Crisz04 <criswalkz04@gmail.com>
2024-10-31 18:49:55 -0500

    borrando un subtitulo

--- EjerciciosIDL3.py
+++ EjerciciosIDL3.py
@@ -2,7 +2,6 @@
 import streamlit as st
 
 st.title("EJERCICIOS PROPUESTOS PARA EL CASO PROPUESTO IDL 3")
-st.subheader("Ejercicio 1: Crear una calculadora de las cuatro operaciones básicas, donde se envíe los parámetros a operar y este retorne los resultados")
 st.subheader("Ejercicio 1: Calculadora con las cuatro operaciones")
 numero1 = st.number_input("Ingrese primer número a operar", min_value = 1)
 numero2 = st.number_input("Ingrese segundo número a operar", min_value = 1)
