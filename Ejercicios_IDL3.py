31fed869bf8db5d283cc511226338fd336e1c915
Crisz04 <criswalkz04@gmail.com>
2024-10-31 18:02:01 -0500

    midificando orden de codigo

--- Ejercicios_IDL3.py
+++ Ejercicios_IDL3.py
@@ -1,16 +1,16 @@
 #EJERCICIOS PARA EL CASO PROPUESTO IDL3
 import streamlit as st
-
-st.title("EJERCICIOS PROPUESTOS PARA EL CASO PROPUESTO IDL 3")
-st.subheader("Ejercicio 1: Calculadora con las cuatro operaciones")
-numero1 = st.number_input("Ingrese primer número a operar", min_value = 1)
-numero2 = st.number_input("Ingrese segundo número a operar", min_value = 1)
-st.write("Selecciona una de las 4 operaciones")
-menus = ["Multiplicar","Dividir","Sumar","Restar"]
-seleccion = ""
-seleccion = st.radio("4 operaciones", menus)
-
 def operaciones():
+
+    st.title("EJERCICIOS PROPUESTOS PARA EL CASO PROPUESTO IDL 3")
+    st.subheader("Ejercicio 1: Calculadora con las cuatro operaciones")
+    numero1 = st.number_input("Ingrese primer número a operar", min_value = 1)
+    numero2 = st.number_input("Ingrese segundo número a operar", min_value = 1)
+    st.write("Selecciona una de las 4 operaciones")
+    menus = ["Multiplicar","Dividir","Sumar","Restar"]
+    seleccion = ""
+    seleccion = st.radio("4 operaciones", menus) 
+
     if seleccion == "Multiplicar":
         resultado = numero1 + numero2
         print(f"El resultado de la suma de es: {numero1} + {numero2} = {resultado}")
